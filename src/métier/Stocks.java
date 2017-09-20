93e37f90a7f5cc83f7939ca4c0147875c03acd37
Unknown <adrien_ferrari@hotmail.com>
2017-09-20 15:08:18 +0200

    Implementation de la classe Machine et Stock

--- src/métier/Stocks.java
+++ src/métier/Stocks.java
@@ -1,5 +1,25 @@
-package mÃ©tier;
+package métier;
 
 public class Stocks {
 
+	int boulon;
+	
+	public void setBoulon(int boulon)
+	{
+		this.boulon = boulon;
+	}
+	public int getBoulon()
+	{
+		return boulon;
+	}
+	
+	public void soustraireBoulon( int boulon)
+	{
+		this.boulon = this.boulon-boulon;
+	}
+	
+	public void ajouterBoulon(int boulon)
+	{
+		this.boulon = this.boulon+boulon;
+	}
 }
