e81c15d3c838c1725e2fa09c091c7995cc919404
BaptisteTr <baptiste.trautmann@etu.unistra.fr>
2017-10-04 09:15:52 +0200

    Test unitaire de passage de commande

--- src/métier/Commande_fournisseur.java
+++ src/métier/Commande_fournisseur.java
@@ -1,5 +1,40 @@
 package métier;
 
+import java.util.Date;
+
 public class Commande_fournisseur {
 
+	double prix;
+	int delai_livraison;
+	Ressource_de_production item;
+	
+	public Commande_fournisseur(double prix, Ressource_de_production item, int delai_livraison){
+		this.prix = prix;
+		this.item = item;
+		this.delai_livraison = delai_livraison;
+	}
+	
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	public int getDelai_livraison() {
+		return delai_livraison;
+	}
+	public void setDelai_livraison(int delai_livraison) {
+		this.delai_livraison = delai_livraison;
+	}
+	public Ressource_de_production getItem() {
+		return item;
+	}
+	public void setItem(Ressource_de_production item) {
+		this.item = item;
+	}
+	
+	
+	public void passerCommande(){
+		
+	}
 }
---
+++ src/métier/Tableau_production.java
@@ -0,0 +1,7 @@
+package métier;
+
+// Visualise les coûts en temps et argent d'une production
+// Est-ce qu'il faut intégrer les 3 éléments d'une production ?? (commande, prod, livraison?)
+public class Tableau_production {
+
+}
